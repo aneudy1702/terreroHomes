2c86aa0ac8fd933501f0ba1ca545de516d057d25
aneudy <elrey1702@gmail.com>
2015-11-05 14:35:51 -0500

    restructured the header and footer

--- php/terrerohomes/wp-content/themes/terrero/footer.php
+++ php/terrerohomes/wp-content/themes/terrero/footer.php
@@ -10,44 +10,135 @@
  */
 ?>
 	</div><!-- #main .wrapper -->
-	<footer id="colophon" role="contentinfo">
+	<!-- <footer id="colophon" role="contentinfo">
 
 		<section class="bottom-container">
-			<ul>
-				<li>
-					<div class="col">
-						<a href="/">
-							<div>Home</div>
-						</a>
-						<a href="/listings">
-							<div>Listings</div>
-						</a>
-						<a href="/contactus">
-							<div>Contact us</div>
-						</a>
-					</div>
-				</li>
-				<li>
-					<div class="col">
-						<div>Jose Terrero</div>
-						<div>jose@terrerohomes.com</div>
-						<div>HERE YOUR ADDRESS</div>
-					</div>
-				</li>
-				<li>
-					<div class="col">
-						Share
-						??
-					</div>
-				</li>
-			</ul>
+
+			<div style="padding-top: 40px; padding-bottom: 40px; margin-left: auto; margin-right: auto; width: 100%;">
+				<table style="width: 100%;">
+					<tbody>
+						<tr>
+							<td class="" style="text-align:center; font-size: 0.88em; width: 241px; vertical-align: top; padding-right: 20px;">
+								<div style="padding-bottom: 12px;"><a style="text-decoration: none; color: #fff;" class="" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">Home</a></div>
+								<div style="padding-bottom: 12px;"><a style="text-decoration: none; color: #fff;" class="" href="/listings">Listings</a></div>
+								<div style="padding-bottom: 12px;"><a style="text-decoration: none; color: #fff;" class="" href="/contactus">Contact us</a></div>
+							</td>
+							<td class="" style="text-align:center; font-size: 0.88em; width: 241px; vertical-align: top; padding-right: 20px;">
+								<div style="padding-bottom: 12px;"><a style="text-decoration: none; color: #fff;" class="" href="#instagramurl">Instagram</a></div>
+								<div style="padding-bottom: 12px;"><a style="text-decoration: none; color: #fff;" class="" href="#facebookurl">Facebook</a></div>
+								<div style="padding-bottom: 12px;"><a style="text-decoration: none; color: #fff;" class="" href="#twitter">Twitter</a></div>
+								<div style="padding-bottom: 12px;"><a style="text-decoration: none; color: #fff;" class="" href="#youremail">Email</a></div>
+							</td>
+							<td class="" style="text-align:center; font-size: 0.88em; width: 241px; vertical-align: top; padding-right: 20px;">
+								<div style="padding-bottom: 12px;"><a style="text-decoration: none; color: #fff;" class="" href="#">Brooklyn Apartments</a></div>
+								<div style="padding-bottom: 12px;"><a style="text-decoration: none; color: #fff;" class="" href="/nyc/kensington-apartments">Bronx Apartments</a></div>
+							</td>
+							<td class="" style="text-align:center; font-size: 0.88em; width: 241px; vertical-align: top;">
+								<div style="padding-bottom: 12px;"><a style="text-decoration: none; color: #fff;" class="" href="#">For Rent Apartments</a></div>
+								<div style="padding-bottom: 12px;"><a style="text-decoration: none; color: #fff;" class="" href="#">For Sale Apartments</a></div>
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
 		</section>
+
+
+
+
 		
 		<div class="site-info">			
 			<a href="http://aneudyabreu.com" title="aneudybreu.com">Developed by aneudyabreu.com from Web Solutions</a>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+		</div><!.site-info -->
+	<!--</footer><! #colophon -->
 </div><!-- #page -->
+
+<footer>
+  <div class="" style="border-top: 1px solid #bbbbbb; border-bottom: 1px solid #bbbbbb; background-color: #383838;">
+    <div style="margin-left: auto; margin-right: auto; width: 1024px; padding-top: 45px; padding-bottom: 60px; color: #bbbbbb;">
+      <div style="float: right;">
+        <div class="font-size-90 bold" style="color: #ffffff; text-align: right;">RentHop.com</div>
+        <div class="font-size-85" style="padding-top: 8px; color: #a0a0a0; text-align: right;">
+          © LM2 Research, Inc.
+        </div>
+        <div class="font-size-85" style="padding-top: 4px; color: #a0a0a0; text-align: right;">
+          Made in New York City
+        </div>
+      </div>
+      <table style="">
+        <tbody>
+          <tr>
+            <td style="vertical-align: top;">
+              <div style="padding-bottom: 8px; color: #ffffff;" class="bold font-size-90">
+                Company
+              </div>
+              <div style="color: #ffffff;" class="font-size-85">
+                <div>
+                  <a style="text-decoration: none; color: #dddddd;" href="/about">About</a>
+                </div>
+                <div style="padding-top: 4px;">
+                  <a style="text-decoration: none; color: #dddddd;" href="http://www.renthop.com/blog">Blog</a>
+                </div>
+              </div>
+            </td>
+            <td style="vertical-align: top; padding-left: 60px;">
+              <div style="padding-bottom: 8px; color: #ffffff;" class="bold font-size-90">
+                Jobs
+              </div>
+              <div style="color: #ffffff;" class="font-size-85">
+                <div>
+                  <a style="text-decoration: none; color: #dddddd;" href="/jobs">We're Hiring!</a>
+                </div>
+              </div>
+            </td>
+            <td style="vertical-align: top; padding-left: 60px;">
+              <div style="padding-bottom: 8px; color: #ffffff;" class="bold font-size-90">
+                Discover
+              </div>
+              <div style="color: #ffffff;" class="font-size-85">
+                <div>
+                  <a style="text-decoration: none; color: #dddddd;" href="/cities/new-york-city-ny">NYC Popular Areas</a>
+                </div>
+                <div style="padding-top: 4px;">
+                  <a style="text-decoration: none; color: #dddddd;" href="/nyc/buildings">NYC Rental Buildings</a>
+                </div>
+                <div style="padding-top: 4px;">
+                  <a style="text-decoration: none; color: #dddddd;" href="/rental-stats-and-trends">Stats and Trends</a>
+                </div>
+              </div>
+            </td>
+            <td style="vertical-align: top; padding-left: 60px;">
+              <div style="padding-bottom: 8px; color: #ffffff;" class="bold font-size-90">
+                Legal
+              </div>
+              <div style="color: #ffffff;" class="font-size-85">
+                <div>
+                  <a style="text-decoration: none; color: #dddddd;" href="/faq">FAQ</a>
+                </div>
+                <div style="padding-top: 4px;">
+                  <a style="text-decoration: none; color: #dddddd;" href="/terms">Terms</a>
+                </div>
+              </div>
+            </td>
+            <td style="vertical-align: top; padding-left: 60px;">
+              <div style="padding-bottom: 8px; color: #ffffff;" class="bold font-size-90">
+                Connect
+              </div>
+              <div style="color: #ffffff;" class="font-size-85">
+                <div style="padding-top: 0px;">
+                  <a style="text-decoration: none; color: #dddddd;" href="https://plus.google.com/+RentHop" rel="publisher">Google+</a>
+                </div>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+      <div style="clear: both;"></div>
+    </div>
+  </div>
+</footer>
+
+
 <!-- <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> -->
 <script type="text/javascript">
 var $ = $ || jQuery;
