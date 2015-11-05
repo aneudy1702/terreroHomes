b193509a2c86cd3ace904ca49f124df175fb5b0c
aneudy <elrey1702@gmail.com>
2015-11-05 14:36:05 -0500

    updated pages header as well

--- php/terrerohomes/wp-content/themes/terrero/header.php
+++ php/terrerohomes/wp-content/themes/terrero/header.php
@@ -32,64 +32,70 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="home-page" class="site">
-	<header id="masthead" class="site-header grid-c" role="banner">
-		<hgroup style="">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</hgroup>
 
-		<nav style="display:none;" id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></button>
-			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
-		</nav><!-- #site-navigation -->
-		
 
-		<div class="full-size-menu">
 
-			<div id="test-nav" class="container">
-			  <div class="row-">
-			    <nav role="navigation">
-			      <span class="entypo-menu" id="toggle-menu"></span>
-			      <!-- <a href="http://localhost:8888/terrerohomes">
-							<div class="logo">
-								<span class="entypo-compass"></span>
-								Terrero Homes
-								<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-							</div>
-			      </a> -->
-			      
-			      <ul>
-			        <li><a href="http://localhost:8888/terrerohomes/contact-us">Contact us</a></li>
-			        <li><a href="http://localhost:8888/terrerohomes/our-agents">Our Agents</a></li>
-			        <li><a href="http://localhost:8888/terrerohomes/about-us">About Us</a></li>			        
-			        <li class="has-child">
-			        	<a 
-			        	
-			        	>Listings</a>
-			          <ul class="dropdown">
-			            <li><a href="http://localhost:8888/terrerohomes/listings?city=bronx">Bronx</a></li>
-			            <li><a href="http://localhost:8888/terrerohomes/listings?city=manhattan">Manhattan</a></li>
-			            <li><a href="http://localhost:8888/terrerohomes/listings?city=queens">Queens</a></li>
-			            <li><a href="http://localhost:8888/terrerohomes/listings?city=brooklyn">Brooklyn</a></li>
-			          </ul>
-			        </li>
-			      </ul>
-			    </nav>
-			  </div>		  
-			</div>
+<header id="masthead" class="site-header grid-c" role="banner">
+	<hgroup style="">
+		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+	</hgroup>
+
+	<nav style="display:none;" id="site-navigation" class="main-navigation" role="navigation">
+		<button class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></button>
+		<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
+		<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
+	</nav><!-- #site-navigation -->
+	
+
+	<div class="full-size-menu">
+
+		<div id="test-nav" class="container">
+		  <div class="row-">
+		    <nav role="navigation">
+		      <span class="entypo-menu" id="toggle-menu"></span>
+		      <!-- <a href="http://localhost:8888/terrerohomes">
+						<div class="logo">
+							<span class="entypo-compass"></span>
+							Terrero Homes
+							<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+						</div>
+		      </a> -->
+		      
+		      <ul>
+		        <li><a href="http://localhost:8888/terrerohomes/contact-us">Contact us</a></li>
+		        <li><a href="http://localhost:8888/terrerohomes/our-agents">Our Agents</a></li>
+		        <li><a href="http://localhost:8888/terrerohomes/about-us">About Us</a></li>			        
+		        <li class="has-child"><a href="http://localhost:8888/terrerohomes/listings?city=bronx">Listings</a>
+		          <ul class="dropdown">
+		            <li><a href="http://localhost:8888/terrerohomes/listings?city=bronx">Bronx</a></li>
+		            <li><a href="http://localhost:8888/terrerohomes/listings?city=manhattan">Manhattan</a></li>
+		            <li><a href="http://localhost:8888/terrerohomes/listings?city=queens">Queens</a></li>
+		            <li><a href="http://localhost:8888/terrerohomes/listings?city=brooklyn">Brooklyn</a></li>
+		          </ul>
+		        </li>
+		        <li>
+	        		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">Home</a>
+		        </li>
+		      </ul>
+		    </nav>
+		  </div>		  
 		</div>
-		<!-- href="http://localhost:8888/terrerohomes/listings?city=bronx" -->
+	</div>
+	
 
-		<!-- <div class="feat-img-cont">
-			<img
-				class="skyline-img size-medium wp-image-24"
-				src="/terrerohomes/wp-content/uploads/2015/10/skyline-night.jpg"
-				alt="nyc-skyline"
-				width="100%"
-			/>
-		</div> -->
-	</header><!-- #masthead -->
+	<!-- <div class="feat-img-cont">
+		<img
+			class="skyline-img size-medium wp-image-24"
+			src="/terrerohomes/wp-content/uploads/2015/10/skyline-night.jpg"
+			alt="nyc-skyline"
+			width="100%"
+		/>
+	</div> -->
+</header><!-- #masthead -->
 
+
+
+
+<div id="home-page" class="site">
 	<div id="main" class="wrapper">
