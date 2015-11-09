cd8b6d1b58dc3535f8c75497f5bbd5b231931501
aneudy <elrey1702@gmail.com>
2015-11-09 11:27:09 -0500

    updated the featured item styles

--- php/terrerohomes/wp-content/themes/terrero/index.php
+++ php/terrerohomes/wp-content/themes/terrero/index.php
@@ -33,7 +33,7 @@
         <!-- slide info -->
 	      <div class="info-container">
 	        
-	        <div class="col">
+	        <!-- <div class="col">
 	        	<div class="prop-title" style="">
 		          1BR at Washington Street
 		        </div>
@@ -73,7 +73,48 @@
 					  </table>
 					</div>
 
-	      </div>
+	      </div> -->
+
+	      <div>
+			    <table style="width: 100%;">
+			      <tbody>
+			        <tr>
+			        	<td>
+			        		<div style="font-size: 1.45em; width: 400px; white-space: nowrap; text-overflow: ellipsis; overflow: hidden;">
+								    1BR at Washington Street
+								  </div>
+								  <div style="font-size: 0.95em; margin-top: 4px; color: #666666; width: 400px; white-space: nowrap; text-overflow: ellipsis; overflow: hidden;">
+								    West Village, Downtown Manhattan, Manhattan
+								  </div>
+			        	</td>
+			          <td>
+			            <div style="font-size: 1.45em; color: #005826; text-align: right;">
+			              $4,365
+			            </div>
+			            <div style="font-size: 0.95em; margin-top: 4px; color: #666666; text-align: right;">
+			              Per Month
+			            </div>
+			          </td>
+			          <td style="width: 20px;">
+			          </td>
+			          <td style="padding-left: 20px; border-left: 1px solid #eeeeee;">
+			            <div
+		              	class="font-size-90 bold view-details-button"
+		              	style="
+		              		margin-right: 10px;
+		              		text-align: center;
+		              		width: 155px;
+		              		padding-top: 8px;
+		              		padding-bottom: 8px;
+		              		cursor: pointer;
+		              		margin-top: 2px;
+		              		display: inline-block;">
+		                <a href="/terrerohomes/property/?p_id=44">View Details</a>
+		              </div>
+			          </td>
+			        </tr>
+			      </tbody>
+			    </table>			  			  
       </div>      
     </section>
     
