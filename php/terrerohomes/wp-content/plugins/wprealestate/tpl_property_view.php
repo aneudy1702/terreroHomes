0db3a3dfa901dbe7d732b11b00feea40939ae0ff
aneudy <elrey1702@gmail.com>
2015-11-14 06:57:07 -0500

    moved the details to the top

--- php/terrerohomes/wp-content/plugins/wprealestate/tpl_property_view.php
+++ php/terrerohomes/wp-content/plugins/wprealestate/tpl_property_view.php
@@ -208,6 +208,243 @@ jQuery(window).load(function() {
   </section>
   <!-- slide show and quick details section -->
 
+
+
+
+<!-- Details section -->
+<section>
+	<div id="content" class="site-content" role="main">
+  	<div class="SpacerDiv"></div>
+  	
+
+  	<?php $property_imgs = get_property_images_ids(false, $post->ID);
+ 
+		if ($property_imgs) { ?>
+    	<div class="ProPhotos">
+      	<!-- Place somewhere in the <body> of your page -->
+      	<div id="slider" class="flexslider">
+        	<ul class="slides">
+          	<?php foreach ($property_imgs as $img_id) { ?>
+            	<li>
+              	<?php echo wp_get_attachment_image($img_id, 'full'); ?>
+            	</li>
+          	<?php } ?>
+            <!-- items mirrored twice, total of 12 -->
+        	</ul>
+      	</div>
+
+
+      	<?php
+
+      	$property_arr_size = count($property_imgs);
+
+				if ($property_arr_size > 1) {
+
+				?>
+        
+	        <div id="carousel" class="flexslider">
+	          <ul class="slides">
+	            <?php 
+
+	            foreach ($property_imgs as $img_id) {
+
+	          	?>
+	              <li>
+	                <?php
+
+	                	echo wp_get_attachment_image($img_id);
+
+	              	?>
+	              </li>
+	              
+	            <?php
+
+	      			}
+
+	    				?>
+
+	          <!-- items mirrored twice, total of 12 -->
+	          </ul>
+	        </div>
+       	<?php } ?>
+    	</div>
+    <?php } ?>
+		
+
+
+		<?php  if (get_option('p_share_buttons') == 1) {?>
+
+      <!-- fb -->
+      <span class='st_fblike_hcount' displayText='<?php _e( ' Facebook Like ', 'wp-realestate' ); ?>'></span>
+      <!-- twt -->
+      <span class='st_twitter_hcount' displayText='<?php _e( ' Tweet ', 'wp-realestate
+      ' ); ?>'></span>
+      <!-- g+ -->
+      <span class='st_googleplus_hcount' displayText='<?php _e( ' Google+ ', 'wp-realestate
+      ' ); ?>'></span>
+
+    <?php } ?>
+        
+    <div class="SpacerDiv"></div>
+
+		<div id="ProDescription">
+
+			<div class="heading">
+				<?php _e( 'Details', 'wp-realestate' ); ?>
+			</div>
+
+		  
+
+
+		  <div class="SpecLabel">		  	
+		  	<?php _e( 'Property Name', 'wp-realestate' ); ?>:
+		      <br>
+		    <?php _e( 'Property Address', 'wp-realestate' ); ?>:
+		  </div>
+
+		  <div class="SpecInfo">
+        <?php 
+					echo get_post_meta($post->ID, 'et_er_property_name', true).'<br>'; 
+					echo get_post_meta($post->ID, 'et_er_address', true).', '.get_post_meta($post->ID, 'et_er_area_location', true).', '.get_post_meta($post->ID, 'et_er_city', true).' '.get_post_meta($post->ID, 'et_er_zipcode', true).' '.get_post_meta($post->ID, 'et_er_state', true);
+				?>
+    	</div>
+
+
+
+      <div class="SpacerDiv"></div>
+
+
+
+      <div>
+        
+        <!-- Ren or Sale -->
+        <div class="SpecLabel">
+          <?php _e( 'Property Type', 'wp-realestate' ); ?>:
+        </div>
+
+        <div class="SpecInfo">
+          <?php echo get_post_meta($post->ID, 'et_er_type', true).'<br />'; ?>
+        </div>
+        
+
+        <!-- Sale Price -->
+        <?php if (get_post_meta($post->ID, 'et_er_price', true) <> '0') { ?>
+
+	        <div class="SpecLabel">
+	          <?php _e( 'Price', 'wp-realestate' ); ?>:
+	        </div>
+
+          <div class="SpecInfo">
+            <?php echo ET_RE_Currency.get_post_meta($post->ID, 'et_er_price', true).'<br />'; ?>
+          </div>
+
+        <?php } ?>
+				
+        <!-- Rent Price -->
+				<?php if (get_post_meta($post->ID, 'et_er_rent_price', true) <> 0) { ?>
+          <div class="SpecLabel">
+            <?php _e( 'Rent', 'wp-realestate' ); ?>: </div>
+          <div class="SpecInfo">
+            <?php echo ET_RE_Currency.get_post_meta($post->ID, 'et_er_rent_price', true).' '. get_post_meta($post->ID, 'et_er_rent_tenure', true) .'<br />'; ?>
+          </div>
+        <?php } ?>
+				
+
+
+      	<!-- Year Built in -->
+				<?php if (get_post_meta($post->ID, 'et_er_built_size', true) <> '0') { ?>
+	        <div class="SpecLabel">
+	          <?php _e( 'Built up', 'wp-realestate' ); ?>: </div>
+	        <div class="SpecInfo">
+	          <?php echo get_post_meta($post->ID, 'et_er_built_size', true).'<br />'; ?>
+	        </div>
+      	<?php } ?>
+
+
+      	<!-- Land Size -->
+				<?php if (get_post_meta($post->ID, 'et_er_land_size', true) <> '0') { ?>
+					<div class="SpecLabel">
+						<?php _e( 'Land area', 'wp-realestate' ); ?>: </div>
+					<div class="SpecInfo">
+						<?php echo get_post_meta($post->ID, 'et_er_land_size', true).'<br />'; ?>
+					</div>
+				<?php } ?>
+				
+
+
+				<!-- Bedrooms -->
+				<?php if (get_post_meta($post->ID, 'et_er_bedroom', true) <> '0') { ?>
+          <div class="SpecLabel">
+            <?php _e( 'Bedroom', 'wp-realestate' ); ?>: </div>
+          <div class="SpecInfo">
+            <?php echo get_post_meta($post->ID, 'et_er_bedroom', true).'<br />'; ?>
+          </div>
+        <?php } ?>
+				
+
+        <!-- BathRoom -->
+				<?php if (get_post_meta($post->ID, 'et_er_bathroom', true) <> '0') { ?>
+          <div class="SpecLabel">
+            <?php _e( 'Bathroom', 'wp-realestate' ); ?>: </div>
+          <div class="SpecInfo">
+            <?php echo get_post_meta($post->ID, 'et_er_bathroom', true).'<br />'; ?>
+          </div>
+        <?php } ?>
+
+
+        <!-- Furnishing -->
+				<?php if (get_post_meta($post->ID, 'et_er_furnishing', true) <> __( 'Not Applicable', 'wp-realestate' )) { ?>
+					<div class="SpecLabel">
+					  <?php _e( 'Furnishing', 'wp-realestate' ); ?>: </div>
+					<div class="SpecInfo">
+					  <?php echo get_post_meta($post->ID, 'et_er_furnishing', true).'<br />'; ?>
+					</div>
+        <?php } ?>
+				
+
+        <!-- Monthly or weekly -->
+				<?php if (get_post_meta($post->ID, 'et_er_tenure', true) <> __( 'Not Applicable', 'wp-realestate' )) { ?>
+	        <div class="SpecLabel">
+	          <?php _e( 'Tenure', 'wp-realestate' ); ?>: </div>
+	        <div class="SpecInfo">
+	          <?php echo get_post_meta($post->ID, 'et_er_tenure', true).'<br />'; ?>
+	        </div>
+        <?php } ?>
+				
+
+        <!-- Date available -->
+				<?php if (get_post_meta($post->ID, 'et_er_date_vacant', true) <> '0') { ?>
+	        <div class="SpecLabel">
+	          <?php _e( 'Date Available', 'wp-realestate' ); ?>: </div>
+	        <div class="SpecInfo">
+	          <?php echo get_post_meta($post->ID, 'et_er_date_vacant', true).'<br />'; ?>
+	        </div>
+        <?php } ?>
+				
+				<?php
+					$terms = get_the_terms( $post->ID, 'facility' );
+					if ( $terms && ! is_wp_error( $terms ) ) {
+				?>
+          <div class="SpecLabel">
+            <?php _e( 'Facilities', 'wp-realestate' ); ?>: </div>
+          <div class="SpecInfo">
+            <?php the_terms( $post->ID, 'facility', '', ', ', ' ' ).'<br />'; ?>
+          </div>
+        <?php } ?>
+  		</div>
+      
+
+
+      <div class="SpacerDiv"></div>
+
+  	</div>
+
+</section>
+<!-- Details Section -->
+
+
+
+
   <!-- description section -->
   <div style="width: 642px; margin-bottom: 35px; padding-bottom: 35px; border-bottom: 1px solid #f2f2f2;">
 	  <div id="ProDescription">
@@ -218,6 +455,9 @@ jQuery(window).load(function() {
 		</div>
 	</div>
 	<!-- description section -->
+
+
+
 
 	<!-- features and amenities  section -->
 	<section>
@@ -327,127 +567,6 @@ jQuery(window).load(function() {
 
 </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<div id="content" class="site-content" role="main">
-<div class="SpacerDiv"></div>
-<?php $property_imgs = get_property_images_ids();
- 
-if ($property_imgs) { ?>
-<div class="ProPhotos">
-<!-- Place somewhere in the <body> of your page -->
-<div id="slider" class="flexslider">
-  <ul class="slides">
-    <?php foreach ($property_imgs as $img_id) { ?>
-    <li>
-      <?php echo wp_get_attachment_image($img_id, 'full'); ?>
-    </li>
-    <?php } ?>
-    
-    <!-- items mirrored twice, total of 12 -->
-  </ul>
-</div>
-<?php $property_arr_size = count($property_imgs);
-	if ($property_arr_size > 1) { ?>
-	    <div id="carousel" class="flexslider">
-  <ul class="slides">
-    <?php
-	foreach ($property_imgs as $img_id) { ?>
-    <li>
-      <?php echo wp_get_attachment_image($img_id); ?>
-    </li>
-    <?php } ?>
-    
-    <!-- items mirrored twice, total of 12 -->
-  </ul>
-</div>
-<?php } ?>
-</div>
-<?php }
-if (get_option('p_share_buttons') == 1) {?>
-<span class='st_fblike_hcount' displayText='<?php _e( 'Facebook Like', 'wp-realestate' ); ?>'></span>
-<span class='st_twitter_hcount' displayText='<?php _e( 'Tweet', 'wp-realestate' ); ?>'></span>
-<span class='st_googleplus_hcount' displayText='<?php _e( 'Google+', 'wp-realestate' ); ?>'></span>
-<span class='st_sharethis_hcount' displayText='<?php _e( 'ShareThis', 'wp-realestate' ); ?>'></span>
-<?php } ?>
-<div class="SpacerDiv"></div>
-<div id="ProDescription">
-<div class="heading"><?php _e( 'Details', 'wp-realestate' ); ?></div>
-<div class="SpecLabel">
-<?php _e( 'Property Name', 'wp-realestate' ); ?>:<br>
-<?php _e( 'Property Address', 'wp-realestate' ); ?>:
-</div>
-<div class="SpecInfo">
-<?php 
-echo get_post_meta($post->ID, 'et_er_property_name', true).'<br>'; 
-echo get_post_meta($post->ID, 'et_er_address', true).', '.get_post_meta($post->ID, 'et_er_area_location', true).', '.get_post_meta($post->ID, 'et_er_city', true).' '.get_post_meta($post->ID, 'et_er_zipcode', true).' '.get_post_meta($post->ID, 'et_er_state', true); ?>
-</div>
-<div class="SpacerDiv"></div>
-
-<div><div class="SpecLabel"><?php _e( 'Property Type', 'wp-realestate' ); ?>: </div>
-
-<div class="SpecInfo"><?php echo get_post_meta($post->ID, 'et_er_type', true).'<br />'; ?></div>
-<?php if (get_post_meta($post->ID, 'et_er_price', true) <> '0') { ?>
-<div class="SpecLabel"><?php _e( 'Price', 'wp-realestate' ); ?>: </div>
-<div class="SpecInfo"><?php echo ET_RE_Currency.get_post_meta($post->ID, 'et_er_price', true).'<br />'; ?></div>
-<?php }
-if (get_post_meta($post->ID, 'et_er_rent_price', true) <> 0) { ?>
-<div class="SpecLabel"><?php _e( 'Rent', 'wp-realestate' ); ?>: </div>
-<div class="SpecInfo"><?php echo ET_RE_Currency.get_post_meta($post->ID, 'et_er_rent_price', true).' '. get_post_meta($post->ID, 'et_er_rent_tenure', true) .'<br />'; ?></div>
-<?php }
-if (get_post_meta($post->ID, 'et_er_built_size', true) <> '0') { ?>
-<div class="SpecLabel"><?php _e( 'Built up', 'wp-realestate' ); ?>: </div>
-<div class="SpecInfo"><?php echo get_post_meta($post->ID, 'et_er_built_size', true).'<br />'; ?></div>
-<?php }
-if (get_post_meta($post->ID, 'et_er_land_size', true) <> '0') { ?>
-<div class="SpecLabel"><?php _e( 'Land area', 'wp-realestate' ); ?>: </div>
-<div class="SpecInfo"><?php echo get_post_meta($post->ID, 'et_er_land_size', true).'<br />'; ?></div>
-<?php }
-if (get_post_meta($post->ID, 'et_er_bedroom', true) <> '0') { ?>
-<div class="SpecLabel"><?php _e( 'Bedroom', 'wp-realestate' ); ?>: </div>
-<div class="SpecInfo"><?php echo get_post_meta($post->ID, 'et_er_bedroom', true).'<br />'; ?></div>
-<?php } 
-if (get_post_meta($post->ID, 'et_er_bathroom', true) <> '0') { ?>
-<div class="SpecLabel"><?php _e( 'Bathroom', 'wp-realestate' ); ?>: </div>
-<div class="SpecInfo"><?php echo get_post_meta($post->ID, 'et_er_bathroom', true).'<br />'; ?></div>
-<?php }
-if (get_post_meta($post->ID, 'et_er_furnishing', true) <> __( 'Not Applicable', 'wp-realestate' )) { ?>
-<div class="SpecLabel"><?php _e( 'Furnishing', 'wp-realestate' ); ?>: </div>
-<div class="SpecInfo"><?php echo get_post_meta($post->ID, 'et_er_furnishing', true).'<br />'; ?></div>
-<?php } 
-if (get_post_meta($post->ID, 'et_er_tenure', true) <> __( 'Not Applicable', 'wp-realestate' )) { ?>
-<div class="SpecLabel"><?php _e( 'Tenure', 'wp-realestate' ); ?>: </div>
-<div class="SpecInfo"><?php echo get_post_meta($post->ID, 'et_er_tenure', true).'<br />'; ?></div>
-<?php } 
-if (get_post_meta($post->ID, 'et_er_date_vacant', true) <> '0') { ?>
-<div class="SpecLabel"><?php _e( 'Date Available', 'wp-realestate' ); ?>: </div>
-<div class="SpecInfo"><?php echo get_post_meta($post->ID, 'et_er_date_vacant', true).'<br />'; ?></div>
-<?php } 
-$terms = get_the_terms( $post->ID, 'facility' );
-if ( $terms && ! is_wp_error( $terms ) ) {
-	?>
-<div class="SpecLabel"><?php _e( 'Facilities', 'wp-realestate' ); ?>: </div>
-<div class="SpecInfo"><?php the_terms( $post->ID, 'facility', '', ', ', ' ' ).'<br />'; ?></div>
-<?php } ?>
-</div>
-<div class="SpacerDiv"></div>
-
-</div>
 
 <div class="SpacerDiv"></div>
 <div class="SpacerDiv"></div>
