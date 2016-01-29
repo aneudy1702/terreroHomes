5bf2f81dbd3ce16cb311ef76c7a127dd510ee806
aneudy <elrey1702@gmail.com>
2016-01-29 00:21:10 -0500

    update(nav-menu: abstracted nav into its own file)

---
+++ php/terrerohomes/wp-content/themes/terrero/sidebar-nav-menu.php
@@ -0,0 +1,12 @@
+<nav role="navigation">
+  <ul>
+    <li><a href="<?php echo esc_url( home_url( '/contact-us' ) ); ?>">Contact us</a></li>
+    <li><a href="<?php echo esc_url( home_url( '/our-agents' ) ); ?>">Our Agents</a></li>
+    <li><a href="<?php echo esc_url( home_url( '/about-us' ) ); ?>">About Us</a></li>
+    <li><a href="<?php echo esc_url( home_url( '/listings' ) ); ?>">Listings</a></li>
+    <li>
+      <a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"
+      rel="home">Home</a>
+    </li>
+  </ul>
+</nav>
