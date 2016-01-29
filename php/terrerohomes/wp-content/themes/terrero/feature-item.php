65c5dc281b62546530578b53c9d8cdccee043f50
aneudy <elrey1702@gmail.com>
2016-01-28 21:56:02 -0500

    update(index: featured-item: abstracted featured item to its own file)

---
+++ php/terrerohomes/wp-content/themes/terrero/feature-item.php
@@ -0,0 +1,70 @@
+<section class="featured-item">
+  <!-- slide show -->
+  <div class="hotel-details-view-container">
+
+    <div class="image-slide-show-container">
+      <div class="slide-show terrero-slideshow">
+        <div class="carousel">
+          <ul>
+            <li>
+              <img src="http://photos.renthop.com/2/6062434_249b2a352e9fe9227c07701671a2d1ac.jpg"
+              alt="" />
+            </li>
+            <li>
+              <img src="http://photos.renthop.com/2/6062434_249b2a352e9fe9227c07701671a2d1ac.jpg"
+              alt="" />
+            </li>
+            <li>
+              <img src="http://photos.renthop.com/2/6062434_249b2a352e9fe9227c07701671a2d1ac.jpg"
+              alt="" />
+            </li>
+            <li>
+              <img src="http://photos.renthop.com/2/6062434_249b2a352e9fe9227c07701671a2d1ac.jpg"
+              alt="" />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <!-- slide info -->
+    <div>
+      <table style="width: 100%;">
+        <tbody>
+          <tr>
+            <td>
+              <div style="font-size: 1.45em; width: 400px; white-space: nowrap; text-overflow: ellipsis; overflow: hidden;">
+                1BR at Washington Street
+              </div>
+              <div style="font-size: 0.95em; margin-top: 4px; color: #666666; width: 400px; white-space: nowrap; text-overflow: ellipsis; overflow: hidden;">
+                West Village, Downtown Manhattan, Manhattan
+              </div>
+            </td>
+            <td>
+              <div style="font-size: 1.45em; color: #005826; text-align: right;">
+                $4,365
+              </div>
+              <div style="font-size: 0.95em; margin-top: 4px; color: #666666; text-align: right;">
+                Per Month
+              </div>
+            </td>
+            <td style="width: 20px;">
+            </td>
+            <td style="padding-left: 20px; border-left: 1px solid #eeeeee;">
+              <div class="font-size-90 bold view-details-button" style="
+				          		margin-right: 10px;
+				          		text-align: center;
+				          		width: 155px;
+				          		padding-top: 8px;
+				          		padding-bottom: 8px;
+				          		cursor: pointer;
+				          		margin-top: 2px;
+				          		display: inline-block;">
+                <a href="/terrerohomes/property/?p_id=44">View Details</a>
+              </div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+</section>
--- php/terrerohomes/wp-content/themes/terrero/index.php
+++ php/terrerohomes/wp-content/themes/terrero/index.php
@@ -5,77 +5,22 @@
 
 <div id="primary" class="site-content home-page">
   <div id="content" role="main">
+    <table class="page-layout-holder">
+      <tbody>
+        <tr>
 
-    <section class="featured-item">
-      <!-- slide show -->
-      <div class="hotel-details-view-container">
-      	
-        <div class="image-slide-show-container">
-          <div class="slide-show terrero-slideshow">
-            <div class="carousel">
-              <ul>
-                <li>
-                  <img src="http://photos.renthop.com/2/6062434_249b2a352e9fe9227c07701671a2d1ac.jpg"
-                  alt="" />
-                </li>
-                <li>
-                  <img src="http://photos.renthop.com/2/6062434_249b2a352e9fe9227c07701671a2d1ac.jpg"
-                  alt="" />
-                </li>
-                <li>
-                  <img src="http://photos.renthop.com/2/6062434_249b2a352e9fe9227c07701671a2d1ac.jpg"
-                  alt="" />
-                </li>
-                <li>
-                  <img src="http://photos.renthop.com/2/6062434_249b2a352e9fe9227c07701671a2d1ac.jpg"
-                  alt="" />
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
+          <td class="col">
+            <?php get_featured_item(); ?>
+          </td>
+            
+          <td class="top">
+            <?php get_sidebar(); ?>
+          </td>
 
-        <!-- slide info -->
-        <div>
-          <table style="width: 100%;">
-            <tbody>
-              <tr>
-                <td>
-                  <div style="font-size: 1.45em; width: 400px; white-space: nowrap; text-overflow: ellipsis; overflow: hidden;">
-                    1BR at Washington Street
-                  </div>
-                  <div style="font-size: 0.95em; margin-top: 4px; color: #666666; width: 400px; white-space: nowrap; text-overflow: ellipsis; overflow: hidden;">
-                    West Village, Downtown Manhattan, Manhattan
-                  </div>
-                </td>
-                <td>
-                  <div style="font-size: 1.45em; color: #005826; text-align: right;">
-                    $4,365
-                  </div>
-                  <div style="font-size: 0.95em; margin-top: 4px; color: #666666; text-align: right;">
-                    Per Month
-                  </div>
-                </td>
-                <td style="width: 20px;">
-                </td>
-                <td style="padding-left: 20px; border-left: 1px solid #eeeeee;">
-                  <div class="font-size-90 bold view-details-button" style="
-				          		margin-right: 10px;
-				          		text-align: center;
-				          		width: 155px;
-				          		padding-top: 8px;
-				          		padding-bottom: 8px;
-				          		cursor: pointer;
-				          		margin-top: 2px;
-				          		display: inline-block;">
-                    <a href="/terrerohomes/property/?p_id=44">View Details</a>
-                  </div>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-    </section>
+        </tr>
+      </tbody>
+    </table>
+    
 
   </div>
     <!-- #content -->
@@ -83,6 +28,4 @@
 
 
 <!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
