ccdc0e5c1c6ae1e6fb346a774bffae0ba7e4bb83
aneudy <elrey1702@gmail.com>
2016-01-28 21:38:04 -0500

    update(sidebar: moved styles from inline to the css file. updated the css selectors and html structure)

--- php/terrerohomes/wp-content/themes/terrero/sidebar.php
+++ php/terrerohomes/wp-content/themes/terrero/sidebar.php
@@ -11,29 +11,32 @@
 ?>
 
 	
-TODO: create dynamic list of mini lisitings
+<!-- TODO: create dynamic list of mini lisitings -->
 
-<div class="mini-search-listing">
-  <table>
-    <tbody>
-      <tr>
-        <td style="vertical-align: top;">
-          <a href="http://www.renthop.com/listings/20_exchange_place/1523/6433125">
-            <div style="background-image: url('http://photos.renthop.com/6/6433125_4b267eecab749ef5a4bc7647bde13c8d.jpg'); background-size: cover; height: 130px; width: 160px; border: 1px solid #23a0ff; border-right: 1px solid #888888;"></div>
-          </a>
-        </td>
-        <td style="padding-left: 20px; vertical-align: top; width: 360px;">
-          <div class="font-size-100 bold" style="padding-bottom: 1px; margin-top: -2px;">
-            <a class="listing-title-link" style="" href="http://www.renthop.com/listings/20_exchange_place/1523/6433125">Studio at 20 Exchange Place</a>
-          </div>
+<div class="mini-listing-list">
+	
+	<!-- repeat this item bellow -->
+	<div class="mini-listing">
+		<table>
+	    <tbody>
+	      <tr>
+	      	<!-- img -->
+	        <td class="mini-col">
+	          <a href="#" alt="link to listing">
+	            <div class="listing-img"></div>
+	          </a>
+	        </td>
 
-
-          <div onclick="" id="check-availability-link" class="" style="">
-            View
-          </div>
-
-        </td>
-      </tr>
-    </tbody>
-  </table>
+	        <!-- mini description -->
+	        <td class="mini-col mini-description">
+	          <div class="title">
+	            <a alt="link to listing" href="#">listing title here</a>
+	          </div>
+	        </td>
+	      </tr>
+	    </tbody>
+	  </table>	
+	</div> 
+	<!-- repeat this item above  -->
+	
 </div>
