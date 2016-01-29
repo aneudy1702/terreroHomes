9ba2dc4808d30fa940c9e1af2e7edab52b68346c
aneudy <elrey1702@gmail.com>
2016-01-28 20:51:52 -0500

    update(added template for mini listings)

--- php/terrerohomes/wp-content/themes/terrero/index.php
+++ php/terrerohomes/wp-content/themes/terrero/index.php
@@ -84,5 +84,5 @@
 
 <!-- #primary -->
 
-<?php //get_sidebar(); ?>
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- php/terrerohomes/wp-content/themes/terrero/sidebar.php
+++ php/terrerohomes/wp-content/themes/terrero/sidebar.php
@@ -10,8 +10,30 @@
  */
 ?>
 
-	<?php if ( is_active_sidebar( 'sidebar-1' ) ) : ?>
-		<div id="secondary" class="widget-area" role="complementary">
-			<?php dynamic_sidebar( 'sidebar-1' ); ?>
-		</div><!-- #secondary -->
-	<?php endif; ?>
+	
+TODO: create dynamic list of mini lisitings
+
+<div class="mini-search-listing">
+  <table>
+    <tbody>
+      <tr>
+        <td style="vertical-align: top;">
+          <a href="http://www.renthop.com/listings/20_exchange_place/1523/6433125">
+            <div style="background-image: url('http://photos.renthop.com/6/6433125_4b267eecab749ef5a4bc7647bde13c8d.jpg'); background-size: cover; height: 130px; width: 160px; border: 1px solid #23a0ff; border-right: 1px solid #888888;"></div>
+          </a>
+        </td>
+        <td style="padding-left: 20px; vertical-align: top; width: 360px;">
+          <div class="font-size-100 bold" style="padding-bottom: 1px; margin-top: -2px;">
+            <a class="listing-title-link" style="" href="http://www.renthop.com/listings/20_exchange_place/1523/6433125">Studio at 20 Exchange Place</a>
+          </div>
+
+
+          <div onclick="" id="check-availability-link" class="" style="">
+            View
+          </div>
+
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</div>
